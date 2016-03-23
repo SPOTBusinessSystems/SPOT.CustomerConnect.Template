f9a44d305cbc6b587eb80a8fb8b7ea02a919ff81
tlewis17 <timl@spotpos.com>
2016-03-23 11:55:29 -0600

    Locker pin is now hidden if the customer doesn't have a locker pin. Added missing 2 way messaging views/dialogs.

---
+++ Template/Default/Components/Dialogs/Message/MessageController.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    'use strict';
+
+    angular
+    .module('app')
+    .controller('MessageController', MessageController);
+
+    MessageController.$inject = ['$rootScope', '$scope', 'dialogs', 'blockUI', 'vcRecaptchaService', 'userService', 'settingsService', 'dataService', '$uibModalInstance', 'data'];
+
+    function MessageController($rootScope, $scope, dialogs, blockUI, vcRecaptchaService, userService, settingsService, dataService, $uibModalInstance, data) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'MessageController';
+
+        activate();
+
+        function activate() {
+            $scope.Reply = false;
+            $scope.Message = data;
+
+            $scope.view = { template: data.Message, tokens: null };
+
+            $scope.close = function () {
+                $uibModalInstance.close();
+            };
+
+            $scope.reply = function () {
+                $scope.Reply = true;
+            };
+
+            $scope.sendReply = function () {
+                dataService.user.sendMessage('RE: ' + $scope.Message.Subject, $scope.messageBody + '\n\r-----------------\n\r' + $scope.Message.Message, null).then(function (data) {
+                    if (data.Failed == false) {
+                        $scope.messageBody = null;
+                        $scope.Reply = false;
+
+                        dialogs.notify('Reply Sent', 'Your reply has been sent.');
+                    } else {
+                        dialogs.error('Messages Error', 'Unable send message.');
+                    }
+                });
+            };
+
+            $scope.delete = function () {
+                dataService.user.deleteMessage($scope.Message.MessageID).then(function (data) {
+                    if (data.Failed == false) {
+                        dataService.user.getMessages().then(function (data) {
+                            if (data.Failed == false) {
+                                console.log(data);
+                                userService.setMessages(data.ReturnObject);
+                            }
+
+                            $scope.close();
+                        });
+                    } else {
+                        dialogs.error('Messages Error', 'Error deleting message.');
+                    }
+                });
+            };
+
+            $scope.cancelReply = function () {
+                $scope.messageBody = null;
+                $scope.Reply = false;
+            };
+        };
+    }
+})();
---
+++ Template/Default/Components/Dialogs/Messages/MessagesController.js
@@ -0,0 +1,84 @@
+﻿(function () {
+    'use strict';
+
+    angular
+    .module('app')
+    .controller('MessagesController', MessagesController);
+
+    MessagesController.$inject = ['$rootScope', '$scope', 'dialogs', 'blockUI', 'vcRecaptchaService', 'userService', 'settingsService', 'dataService', '$uibModalInstance'];
+
+    function MessagesController($rootScope, $scope, dialogs, blockUI, vcRecaptchaService, userService, settingsService, dataService, $uibModalInstance) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'MessagesController';
+
+        activate();
+
+        function activate() {
+            $scope.unreadMessages = "";
+            $scope.filteredMessages = [];
+            $scope.itemsPerPage = 10;
+            $scope.currentPage = 1;
+
+            $scope.numPages = function () {
+                return Math.ceil($scope.Messages.length / $scope.numPerPage);
+            };
+
+            $scope.figureOutMessagesToDisplay = function () {
+                var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+                var end = begin + $scope.itemsPerPage;
+                $scope.filteredMessages = [];
+                if ($scope.Messages != null && $scope.Messages.length > 0) {
+                    $scope.filteredMessages = $scope.Messages.slice(begin, end);
+                }
+            };
+
+            $scope.pageChanged = function () {
+                $scope.figureOutMessagesToDisplay();
+            };
+
+            $scope.close = function () {
+                $scope.filteredMessages = [];
+                $scope.selectedMessage = null;
+                $uibModalInstance.close();
+            };
+
+            $scope.open = function (index) {
+                console.log($scope.filteredMessages[index]);
+                dataService.user.readMessage($scope.filteredMessages[index].MessageID).then(function (data) {
+                    
+                });
+
+                var dlg = dialogs.create(settingsService.path + 'Components/Dialogs/Message/Message.html', 'MessageController', $scope.filteredMessages[index], 'md').result.then(function () {
+                    $scope.LoadMessages();
+                });
+            }
+
+            $scope.refresh = function () {
+                dataService.user.getMessages().then(function (data) {
+                    if (!data.Failed) {
+                        userService.setMessages(data.ReturnObject);
+                        $scope.LoadMessages();
+                    } else {
+                        dialogs.error('Messages Error', 'Unable to load messages.')
+                    }
+                });
+            }
+
+            $scope.LoadMessages = function () {
+                $scope.selectedMessage = null;
+                
+                dataService.user.getMessages().then(function (data) {
+                    if (data.Failed == false) {
+                        userService.setMessages(data.ReturnObject);
+                        $scope.Messages = userService.getMessages();
+                        $scope.unreadMessages = userService.unreadMessageCount();
+                        $scope.figureOutMessagesToDisplay();
+                    }
+                });
+            };
+
+            $scope.LoadMessages();
+        };
+    }
+})();
